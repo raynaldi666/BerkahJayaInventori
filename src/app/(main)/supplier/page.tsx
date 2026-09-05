5d4edbcc924a0a8ab1f56ddb104810377f095295
Zidan <zidanewibowo08@gmail.com>
2026-09-05 23:41:45 +0700

    ketinggalan

--- src/app/(main)/supplier/page.tsx
+++ src/app/(main)/supplier/page.tsx
@@ -152,7 +152,7 @@ export default function MasterSupplier() {
 
           <div className="form-row">
             <div className="form-group">
-              <label className="form-label">Jenis Material</label>
+              <label className="form-label">Kelompok</label>
               <select className="input" value={jenisSupplier} onChange={e => setJenisSupplier(e.target.value)}>
                 <option>Semen & Bahan Dasar</option>
                 <option>Cat & Pelapis</option>
